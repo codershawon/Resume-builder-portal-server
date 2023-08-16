28b3ae97e9e5db254e35fb4cfc203d3254d7ff22
sabitameem <sabitameem578@gmail.com>
2023-08-17 00:48:21 +0530

    did some change

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const express = require('express')
+const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5abjn4e.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -16,7 +15,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,17 +23,50 @@ async function run() {
     // Connect the client to the server    (optional starting in v4.7)
     // await client.connect();
 
-
-//commented by meem 
     // Connect the client to the server	(optional starting in v4.7)
 
-// user collection
-    const usersCollection=client.db("resumeBuilderPortal").collection("users")
+    // user collection
+    const usersCollection = client
+      .db("resumeBuilderPortal")
+      .collection("users");
+
+    
+    //user related routes
+
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user?.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+  
+
+
+
+
+
+
+
+
 
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -42,10 +74,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Resume builder portal server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Resume builder portal server is running");
+});
 
 app.listen(port, () => {
-  console.log(`Resume builder portal server is running on port ${port}`)
-})
+  console.log(`Resume builder portal server is running on port ${port}`);
+});
