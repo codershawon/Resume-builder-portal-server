2494e1b18fb13e3d16886a64796588e9ec337657
sabitameem <sabitameem578@gmail.com>
2023-08-18 00:47:14 +0530

    added jwt usersCollection api

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 console.log(process.env.ACCESS_TOKEN_SECRET)
+
+
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5abjn4e.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -40,14 +63,14 @@ async function run() {
   app.post("/jwt",(req,res)=>{
     const user=req.body
     console.log(user)
-    const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: 5 })
+    const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: "1h" })
     console.log(token)
     res.send({token})
   })
 
     //user related routes
-
-    app.get("/users", async (req, res) => {
+    //  TODO : add verifyJWT
+    app.get("/users",verifyJWT, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -64,6 +87,49 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+
+      //admin
+       //  TODO : add verifyJWT
+      app.get("/users/admin/:email",verifyJWT,  async (req, res) => {
+        const email = req.params.email;
+        if (req.decoded?.email !== email) {
+          res.send({ admin: false });
+          return;
+        }
+        const query = { email: email };
+        const user = await usersCollection.findOne(query);
+        const result = { admin: user?.role === "admin" };
+        res.send(result);
+      });
+
+
+      app.patch("/users/admin/:id", async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const filter = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //user Reviews routes
     app.get("/review", async (req, res) => {
