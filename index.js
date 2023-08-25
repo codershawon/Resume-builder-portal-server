1cdd1dae823a7f1b1a7088e1de4523a6db58d5b8
CoderShawon <shawonb500@gmail.com>
2023-08-25 19:12:02 +0600

    conflict fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
-const stripe = require("stripe")(process.env.Payment_Secrect_Key);
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -33,7 +33,7 @@ const verifyJWT = (req, res, next) => {
   });
 };
 
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.5abjn4e.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5abjn4e.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
